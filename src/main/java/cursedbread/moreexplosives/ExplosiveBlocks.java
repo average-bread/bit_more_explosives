1f5a85c799ee93a95f39ada66420c852184c0211
average-bread <163648686+average-bread@users.noreply.github.com>
2024-12-05 20:11:03 +0100

    a

--- src/main/java/cursedbread/moreexplosives/ExplosiveBlocks.java
+++ src/main/java/cursedbread/moreexplosives/ExplosiveBlocks.java
@@ -5,11 +5,18 @@ import net.minecraft.client.render.block.model.BlockModelStandard;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockTNT;
 import net.minecraft.core.block.material.Material;
+import net.minecraft.core.sound.BlockSound;
 import turniplabs.halplibe.helper.BlockBuilder;
 
 import static cursedbread.moreexplosives.ExplosiveMain.MOD_ID;
 
 public class ExplosiveBlocks {
+
+	BlockBuilder modtnt = new BlockBuilder(MOD_ID)
+		.setBlockSound(new BlockSound("step.grass", "step.grass", 1.0f, 1.0f))
+		.setHardness(0.0f)
+		.setResistance(0.0f)
+		.setBlockModel(BlockModelStandard::new);
 
 	public static int blockId;
 
@@ -19,11 +26,9 @@ public class ExplosiveBlocks {
 
 	}
 	public void initilizeBlocks() {
-		x8Tnt = new BlockBuilder(MOD_ID)
-			.setBlockModel(block -> new BlockModelStandard<>(block)
-				.withTextures("moreexplosives:block/x8_tnt_top", "moreexplosives:block/x8_tnt_bottom", "moreexplosives:block/x8_tnt_side")
-			)
-				.build(new BlockX8TNT("x8Tnt", blockId++));
+		x8Tnt = modtnt
+			.setBlockModel(block -> new BlockModelStandard<>(block).withTextures("moreexplosives:block/x8_tnt_top", "moreexplosives:block/x8_tnt_bottom", "moreexplosives:block/x8_tnt_side"))
+			.build(new BlockX8TNT("x8Tnt", blockId++));
 
 		initializeBlockDetails();
 	}
